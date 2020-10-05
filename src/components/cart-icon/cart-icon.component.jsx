be80b43f02125e7549b9c790fe668a92ea11c66d
David Chavarría <dabbidchavarria@gmail.com>
2020-10-05 13:43:57 -0600

    Converted cartDropdown, cart-icon and cart-item to style-components

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,15 +6,13 @@ import toggleCartHidden from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
 
-import {ReactComponent as ShoppingIcon} from './../../assets/shopping-bag.svg.svg';
-
-import './cart-icon.styles.scss';
+import {CartIconContainer, ShoppingIconContainer, ItemCountContainer} from './cart-icon.styles';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
-        <ShoppingIcon className='shopping-icon'></ShoppingIcon>
-        <span className='item-count'> {itemCount} </span>
-    </div>
+    <CartIconContainer onClick={toggleCartHidden}>
+        <ShoppingIconContainer></ShoppingIconContainer>
+        <ItemCountContainer> {itemCount} </ItemCountContainer>
+    </CartIconContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import './cart-item.styles.scss';
+import {CartItemContainer, ImgContainer, ItemsDetailsContainer, DetailContainer} from './cart-item.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-    <div className='cart-item'>
-        <img src={imageUrl} alt="item" />
-        <div className='item-details'>
-            <span className='name'>{name}</span>
-            <span className='price'>{quantity} x ${price}</span>
-        </div>
-    </div>
+    <CartItemContainer>
+        <ImgContainer src={imageUrl} alt="item" />
+        <ItemsDetailsContainer>
+            <DetailContainer>{name}</DetailContainer>
+            <DetailContainer>{quantity} x ${price}</DetailContainer>
+        </ItemsDetailsContainer>
+    </CartItemContainer>
 );
 
 export default CartItem;
